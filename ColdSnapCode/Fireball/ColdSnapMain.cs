81480476ec825accb52ff22b3ed67f156ebb34d3
Lilian Ready <dezingerlily@gmail.com>
2024-02-20 12:12:03 -0500

    Regurgitate

--- ColdSnapCode/Fireball/ColdSnapMain.cs
+++ ColdSnapCode/Fireball/ColdSnapMain.cs
@@ -10,6 +10,8 @@ using Expedition;
 using JollyCoop;
 using RWCustom;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static Player;
 
 
 namespace Fireball
@@ -23,8 +25,9 @@ namespace Fireball
         public const string PLUGIN_VERSION = "1.0.0";
 
         public static int fireBalls = 0;
-        public bool metIterator = false;
+        public bool metIterator = true;
 
+        private AbstractPhysicalObject fireEntity;
         public class firingState
         {
             public Vector2 direction;
@@ -62,6 +65,7 @@ namespace Fireball
 
         private void OnEnable()
         {
+            
             On.Player.SwallowObject += SnowSwallowObject;
             On.Player.Regurgitate += NoRegurge;
             On.Player.ThrowObject += Fire;
@@ -69,7 +73,7 @@ namespace Fireball
 
         private void Fire(On.Player.orig_ThrowObject orig, Player self, int grasp, bool eu)
         {
-            if (self.SlugCatClass == new SlugcatStats.Name("SnowCat"))
+            if (self.SlugCatClass == new SlugcatStats.Name("SnowCat") && metIterator)
             {
             }
             else
@@ -78,11 +82,11 @@ namespace Fireball
             }
         }
 
-        void SnowSwallowObject(On.Player.orig_SwallowObject orig, Player self, int grasp)
+        private void SnowSwallowObject(On.Player.orig_SwallowObject orig, Player self, int grasp)
         {
             //Registering didn't work, even moving this stuff over to slugbase
             //so create the name when you check it
-            if (self.SlugCatClass == new SlugcatStats.Name("SnowCat"))
+            if (self.SlugCatClass == new SlugcatStats.Name("SnowCat") && metIterator)
             {
                 //This is mostly the normal code just updated for our dude and our level of access
                 //Removed any reference to objectInStomach to prevent storage
@@ -114,11 +118,67 @@ namespace Fireball
             }
 
         }
-        void NoRegurge(On.Player.orig_Regurgitate orig, Player self)
+        private void NoRegurge(On.Player.orig_Regurgitate orig, Player self)
         {
             //If our cat tries to throw up don't do anything
-            if (self.SlugCatClass == new SlugcatStats.Name("SnowCat"))
+            if (self.SlugCatClass == new SlugcatStats.Name("SnowCat") && metIterator)
             {
+                if(fireBalls > 0)
+                {
+                    //Basically the same that the base has, but adjusted for only spitting out fireballs.
+                    //Add fireball to room
+                    self.room.abstractRoom.AddEntity(fireEntity);
+                    fireEntity.pos = self.abstractCreature.pos;
+                    fireEntity.RealizeInRoom();
+                    Vector2 pos = self.bodyChunks[0].pos;
+                    Vector2 vector = Custom.DirVec(self.bodyChunks[1].pos, self.bodyChunks[0].pos);
+                    bool flag = false;
+                    if (Mathf.Abs(self.bodyChunks[0].pos.y - self.bodyChunks[1].pos.y) > Mathf.Abs(self.bodyChunks[0].pos.x - self.bodyChunks[1].pos.x) && self.bodyChunks[0].pos.y > self.bodyChunks[1].pos.y)
+                    {
+                        pos += Custom.DirVec(self.bodyChunks[1].pos, self.bodyChunks[0].pos) * 5f;
+                        vector *= -1f;
+                        vector.x += 0.4f * (float)self.flipDirection;
+                        vector.Normalize();
+                        flag = true;
+                    }
+                    fireEntity.realizedObject.firstChunk.HardSetPosition(pos);
+                    fireEntity.realizedObject.firstChunk.vel = Vector2.ClampMagnitude((vector * 2f + Custom.RNV() * UnityEngine.Random.value) / fireEntity.realizedObject.firstChunk.mass, 6f);
+                    self.bodyChunks[0].pos -= vector * 2f;
+                    self.bodyChunks[0].vel -= vector * 2f;
+                    if (self.graphicsModule != null)
+                    {
+                        (self.graphicsModule as PlayerGraphics).head.vel += Custom.RNV() * UnityEngine.Random.value * 3f;
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        self.room.AddObject(new WaterDrip(pos + Custom.RNV() * UnityEngine.Random.value * 1.5f, Custom.RNV() * 3f * UnityEngine.Random.value + vector * Mathf.Lerp(2f, 6f, UnityEngine.Random.value), waterColor: false));
+                    }
+                    self.room.PlaySound(SoundID.Slugcat_Regurgitate_Item, self.mainBodyChunk);
+                    if (fireEntity.realizedObject is Hazer && self.graphicsModule != null)
+                    {
+                        (fireEntity.realizedObject as Hazer).SpitOutByPlayer(PlayerGraphics.SlugcatColor(self.playerState.slugcatCharacter));
+                    }
+                    if (flag && self.FreeHand() > -1)
+                    {
+                        if (ModManager.MMF && ((self.grasps[0] != null) ^ (self.grasps[1] != null)) && self.Grabability(fireEntity.realizedObject) == ObjectGrabability.BigOneHand)
+                        {
+                            int num = 0;
+                            if (self.FreeHand() == 0)
+                            {
+                                num = 1;
+                            }
+                            if (self.Grabability(self.grasps[num].grabbed) != ObjectGrabability.BigOneHand)
+                            {
+                                self.SlugcatGrab(fireEntity.realizedObject, self.FreeHand());
+                            }
+                        }
+                        else
+                        {
+                            self.SlugcatGrab(fireEntity.realizedObject, self.FreeHand());
+                        }
+                    }
+                    fireBalls--;
+                }    
                 return;
             }
             else
