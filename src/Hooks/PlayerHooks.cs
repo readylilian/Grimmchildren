5b412808a4aaf01dd52ff4571acb92540ba89af9
Lilian Ready <dezingerlily@gmail.com>
2024-02-28 17:11:31 -0500

    This version of fire is working completely

--- src/Hooks/PlayerHooks.cs
+++ src/Hooks/PlayerHooks.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 
-namespace SlugTemplate.Hooks;
-
-public static class PlayerHooks
+namespace SlugTemplate.Hooks
 {
-    public static void Init()
-    {
-        // example of a hook
-        On.Player.Jump += Player_Jump;
-    }
 
-    private static void Player_Jump(On.Player.orig_Jump orig, Player self)
+    public static class PlayerHooks
     {
-        // Call the original method we hook from first
-        orig(self);
-        
-        // Adding power to jump
-        self.jumpBoost *= 1f + 0.5f;
-        
-        
-        // Currently, we don't have an if statement to specify which slugcat this happens to
-        // This means it happens to all of them, in every campaign for each jump
+        public static void Init()
+        {
+            // example of a hook
+            On.Player.Jump += Player_Jump;
+        }
+
+        private static void Player_Jump(On.Player.orig_Jump orig, Player self)
+        {
+            // Call the original method we hook from first
+            orig(self);
+
+            // Adding power to jump
+            self.jumpBoost *= 1f + 0.5f;
+
+
+            // Currently, we don't have an if statement to specify which slugcat this happens to
+            // This means it happens to all of them, in every campaign for each jump
+        }
     }
 }
--- src/Options.cs
+++ src/Options.cs
@@ -3,49 +3,50 @@ using Menu.Remix.MixedUI;
 using Menu.Remix.MixedUI.ValueTypes;
 using UnityEngine;
 
-namespace SlugTemplate;
-
-public class Options : OptionInterface
+namespace SlugTemplate
 {
-    private readonly ManualLogSource Logger;
-    
-    public Options(Plugin modInstance, ManualLogSource loggerSource)
+    public class Options : OptionInterface
     {
-        Logger = loggerSource;
-        
-        // Bind a key to the option, as well as a default value
-        testOption = config.Bind("test", false);
-    }
-    
-    public readonly Configurable<bool> testOption;
-    private UIelement[] UIArrPlayerOptions;
+        private readonly ManualLogSource Logger;
 
-
-    public override void Initialize()
-    {
-        var opTab = new OpTab(this, "Options");
-        
-        // Adds created tabs to remix menu
-        Tabs = new[]
+        public Options(Plugin modInstance, ManualLogSource loggerSource)
         {
+            Logger = loggerSource;
+
+            // Bind a key to the option, as well as a default value
+            testOption = config.Bind("test", false);
+        }
+
+        public readonly Configurable<bool> testOption;
+        private UIelement[] UIArrPlayerOptions;
+
+
+        public override void Initialize()
+        {
+            var opTab = new OpTab(this, "Options");
+
+            // Adds created tabs to remix menu
+            Tabs = new[]
+            {
             opTab
         };
 
-        // Painstakingly define the layout of the tab
-        UIArrPlayerOptions = new UIelement[]
-        {
+            // Painstakingly define the layout of the tab
+            UIArrPlayerOptions = new UIelement[]
+            {
             new OpLabel(10f, 550f, "Options", true),
             new OpLabel(10f, 520f, "Test Option")
             {
                 description = "This option should always be saved if confirmed, and is accessible in other classes"
             },
             new OpCheckBox(testOption, 105f, 520f)
-        };
-        opTab.AddItems(UIArrPlayerOptions);
-    }
+            };
+            opTab.AddItems(UIArrPlayerOptions);
+        }
 
-    public override void Update()
-    {
-        // If you want anything to change in view based on the options (color/image/etc.), do that here
+        public override void Update()
+        {
+            // If you want anything to change in view based on the options (color/image/etc.), do that here
+        }
     }
 }
