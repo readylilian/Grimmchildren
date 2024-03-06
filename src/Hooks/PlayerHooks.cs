f9f3b159e2fa4285be36ba601789eb3b8853b98b
Zachruff <zachary.rubin02@gmail.com>
2024-03-06 11:00:01 -0500

    Removed jump modifier hook

--- src/Hooks/PlayerHooks.cs
+++ src/Hooks/PlayerHooks.cs
@@ -15,25 +15,25 @@ public static class PlayerHooks
     public static void Init()
     {
         // example of a hook
-        On.Player.Jump += Player_Jump;
+        //On.Player.Jump += Player_Jump;
 
         AssetLoader.LoadAssets();
         ApplyPlayerGraphicsHooks();
         //MenuDepthIllustrationHook();
     }
 
-    private static void Player_Jump(On.Player.orig_Jump orig, Player self)
-    {
+    //private static void Player_Jump(On.Player.orig_Jump orig, Player self)
+    //{
         // Call the original method we hook from first
-        orig(self);
+        //orig(self);
 
         // Adding power to jump
-        self.jumpBoost *= 1f + 0.5f;
+        //self.jumpBoost *= 1f + 0.5f;
 
 
         // Currently, we don't have an if statement to specify which slugcat this happens to
         // This means it happens to all of them, in every campaign for each jump
-    }
+    //}
 
 /*    public static void MenuDepthIllustrationHook()
     {
