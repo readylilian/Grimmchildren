5552acd31fb9eb24f0d5bca90d2356ac00cd3785
Elumenix <dps5393@g.rit.edu>
2024-03-25 22:13:20 -0400

    Got collisions and snow separated appropriately

--- src/Ice Block/IceBlockData.cs
+++ src/Ice Block/IceBlockData.cs
@@ -12,20 +12,18 @@ public class IceBlockData : Pom.ManagedData
 
     private static readonly Pom.ManagedField[] newFields = new Pom.ManagedField[]
     {
-        new Pom.Vector2Field("HitboxPivot", new Vector2(-150f, 0f), Pom.Vector2Field.VectorReprType.none),
-        new Pom.DrivenVector2Field("ev2", "HitboxPivot", new Vector2(-100, -40), 
-            Pom.DrivenVector2Field.DrivenControlType.relativeLine, "Height"),
-        //new Pom.Vector2Field("ev2", new Vector2(-100f, -40f), Pom.Vector2Field.VectorReprType.none, null),
-        new Pom.DrivenVector2Field("ev3", "ev2", new Vector2(-100f, 0f),
-            Pom.DrivenVector2Field.DrivenControlType.relativeLine, "Rotation and Width"),
+        new Pom.Vector2Field("ev2", new Vector2(-100f, -40f), Pom.Vector2Field.VectorReprType.none, null),
+        new Pom.DrivenVector2Field("ev3", "ev2", new Vector2(-100f, -40f),
+            Pom.DrivenVector2Field.DrivenControlType.rectangle, null),
+        new Pom.Vector2Field("SnowPivot", new Vector2(-100f, 0f), Pom.Vector2Field.VectorReprType.none),
         new Pom.FloatField("Intensity", 0f, 100f, 100f),
         new Pom.ExtEnumField<PlacedObject.SnowSourceData.Shape>("Shape", PlacedObject.SnowSourceData.Shape.Radial),
-        new Pom.Vector2Field("Radius", 0, 100, Pom.Vector2Field.VectorReprType.circle)
+        new Pom.DrivenVector2Field("Radius", "SnowPivot", new Vector2(0, 100), Pom.DrivenVector2Field.DrivenControlType.perpendicularOval)
     };
     
     [BackedByField("ev2")] public Vector2 xHandle;
     [BackedByField("ev3")] public Vector2 yHandle;
-    [BackedByField("HitboxPivot")] public Vector2 HitboxPivot;
+    [BackedByField("SnowPivot")] public Vector2 snowPivot;
     [BackedByField("Radius")] public Vector2 radHandle;
     [BackedByField("Intensity")] public float intensity;
     [BackedByField("Shape")] public PlacedObject.SnowSourceData.Shape shape;
