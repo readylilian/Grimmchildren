97b9847fddb3eb9ca0a0503e8f00411778dcec41
Elumenix <dps5393@g.rit.edu>
2024-03-25 21:36:23 -0400

    Got snow and collision apart

--- src/Ice Block/IceBlockData.cs
+++ src/Ice Block/IceBlockData.cs
@@ -12,16 +12,21 @@ public class IceBlockData : Pom.ManagedData
 
     private static readonly Pom.ManagedField[] newFields = new Pom.ManagedField[]
     {
-        new Pom.Vector2Field("ev2", new Vector2(-100f, -40f), Pom.Vector2Field.VectorReprType.none, null),
-        new Pom.DrivenVector2Field("ev3", "ev2", new Vector2(-100f, -40f),
-            Pom.DrivenVector2Field.DrivenControlType.rectangle, null),
+        new Pom.Vector2Field("HitboxPivot", new Vector2(-150f, 0f), Pom.Vector2Field.VectorReprType.none),
+        new Pom.DrivenVector2Field("ev2", "HitboxPivot", new Vector2(-100, -40), 
+            Pom.DrivenVector2Field.DrivenControlType.relativeLine, "Height"),
+        //new Pom.Vector2Field("ev2", new Vector2(-100f, -40f), Pom.Vector2Field.VectorReprType.none, null),
+        new Pom.DrivenVector2Field("ev3", "ev2", new Vector2(-100f, 0f),
+            Pom.DrivenVector2Field.DrivenControlType.relativeLine, "Rotation and Width"),
         new Pom.FloatField("Intensity", 0f, 100f, 100f),
-        new Pom.FloatField("Irregularity", 0f, 100f, 0f),
         new Pom.ExtEnumField<PlacedObject.SnowSourceData.Shape>("Shape", PlacedObject.SnowSourceData.Shape.Radial),
         new Pom.Vector2Field("Radius", 0, 100, Pom.Vector2Field.VectorReprType.circle)
     };
     
     [BackedByField("ev2")] public Vector2 xHandle;
     [BackedByField("ev3")] public Vector2 yHandle;
+    [BackedByField("HitboxPivot")] public Vector2 HitboxPivot;
     [BackedByField("Radius")] public Vector2 radHandle;
+    [BackedByField("Intensity")] public float intensity;
+    [BackedByField("Shape")] public PlacedObject.SnowSourceData.Shape shape;
 }
--- src/Ice Block/PlacedIceBlock.cs
+++ src/Ice Block/PlacedIceBlock.cs
@@ -1,4 +1,6 @@
-﻿using MoreSlugcats;
+﻿using System;
+using DevInterface;
+using MoreSlugcats;
 using RWCustom;
 using UnityEngine;
 
@@ -10,9 +12,7 @@ public class PlacedIceBlock : SnowSource
 
     public PlacedIceBlock(PlacedObject owner, Room room) : base(owner.pos)
     {
-        //IceBlockData iceBlockData = owner.data as IceBlockData;
         _po = owner;
-        //new SnowSource()
     }
 
     private IceBlockData _Data
@@ -26,25 +26,46 @@ public class PlacedIceBlock : SnowSource
 
     public override void Update(bool eu)
     {
+        // Pass over values as updated in dev mode
         pos = _po.pos;
         rad = _Data.radHandle.magnitude;
+        shape = _Data.shape;
+        intensity = _Data.intensity;
+        
+        // Make sure that height is always 90 degrees counter clockwise
+        var magnitude = _Data.xHandle.magnitude;
+        _Data.xHandle = _Data.xHandle.normalized;
+        _Data.xHandle = Vector2.Perpendicular(_Data.yHandle);
+        //_Data.xHandle = new Vector2(500, 500);
+        _Data.fields[2] as DevUINode
+        _Data.SetValue("xHandle", new Vector2(500, 500));
+        
         base.Update(eu);
         
         
-        Rect areaRect = new Rect(_po.pos.x, _po.pos.y, _Data.xHandle.magnitude, _Data.yHandle.magnitude);
+        Rect areaRect = new Rect(_po.pos.x + _Data.HitboxPivot.x, _po.pos.y + _Data.HitboxPivot.y, _Data.xHandle.magnitude, _Data.yHandle.magnitude);
         Vector2 center = areaRect.center;
         float angle = Custom.VecToDeg(_Data.yHandle);
+        Console.WriteLine("Center = " + center + ", Angle = " + angle + ", Width = " + _Data.xHandle.magnitude +
+                          ", Height = " + _Data.yHandle.magnitude);
+
+        
         foreach (UpdatableAndDeletable obj in room.updateList)
         {
-            if (obj is PhysicalObject p)
+            // TODO: Remove Player line
+            if (obj is PhysicalObject p && p is Player)
             {
                 foreach (BodyChunk chunk in p.bodyChunks)
                 {
-                    Vector2 rotatedPosition = Custom.RotateAroundVector(chunk.pos, _po.pos, -angle);
+                    Vector2 rotatedPosition = Custom.RotateAroundVector(chunk.pos, _Data.HitboxPivot + _po.pos, -angle);
+                    Console.WriteLine("RotatedPosition = " + rotatedPosition.ToString());
                     Vector2 centerBias = (center - rotatedPosition).normalized * 0.01f;
                     Vector2 collisionCandidate =
-                        Custom.RotateAroundVector(areaRect.GetClosestInteriorPoint(rotatedPosition), _po.pos, angle);
+                        Custom.RotateAroundVector(areaRect.GetClosestInteriorPoint(rotatedPosition), _Data.HitboxPivot + _po.pos, angle);
+                    Console.WriteLine("CollisionCandidate = " + collisionCandidate.ToString());
                     centerBias = Custom.RotateAroundOrigo(centerBias, angle);
+                    Console.WriteLine("centerBias = " + centerBias);
+                    
                     p.PushOutOf(collisionCandidate + centerBias, 0f, -1);
                 }
             }
