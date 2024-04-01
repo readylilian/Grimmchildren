da22636dc32878ba39356d2e7519273b657a9883
Elumenix <dps5393@g.rit.edu>
2024-03-31 20:19:45 -0400

    Puzzle 1 script mostly completed

--- src/Hooks/RoomScripts.cs
+++ src/Hooks/RoomScripts.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using MoreSlugcats;
 using RWCustom;
@@ -26,17 +27,25 @@ public static class RoomScripts
 
     public sealed class PuzzleRoomEnergyCell : UpdatableAndDeletable
 	{
+		private EnergyCell myEnergyCell;
+		public Player player;
+		private bool primed;
+		private EnergyCell foundCell;
+		private bool finalPhase;
+		private bool lethalMode;
+		private int noCellPresenceTime;
+		
 		public PuzzleRoomEnergyCell(Room room)
 		{
 			this.room = room;
             primed = false;
-            IntVector2 pos = new IntVector2(225, 255); // Goal
+            IntVector2 pos = new IntVector2(14, 13); // Goal
             
             // Block for if the orb is already at the goal of the level
             if (this.room.game.session is StoryGameSession /*&& TODO: Save Data check*/)
             {
 	            // Spawn Cell at goal
-	            AbstractPhysicalObject abstractPhysicalObject = new AbstractPhysicalObject(this.room.world,
+	           /* AbstractPhysicalObject abstractPhysicalObject = new AbstractPhysicalObject(this.room.world,
 		            MoreSlugcatsEnums.AbstractObjectType.EnergyCell, null, this.room.GetWorldCoordinate(pos),
 		            this.room.game.GetNewID())
 	            {
@@ -57,7 +66,7 @@ public static class RoomScripts
 	            RoomSettings.RoomEffect item = new RoomSettings.RoomEffect(RoomSettings.RoomEffect.Type.ZeroG, 1f, false);
             	this.room.roomSettings.effects.Add(item);
             	lethalMode = true;
-            	return;
+            	return;*/
             }
 
             // Make sounds play for active orb
@@ -101,25 +110,135 @@ public static class RoomScripts
 				myEnergyCell = (abstractPhysicalObject.realizedObject as EnergyCell);
 				myEnergyCell!.firstChunk.pos = vector;
 			}
+			
+			
+			// Cell has already been placed at goal
+			if (lethalMode)
+			{
+				vector = room.MiddleOfTile(new IntVector2(14, 13));
+				if (foundCell != null)
+				{
+					foundCell.customAnimation = true;
+					foundCell.moveToTarget = 0.9f;
+					foundCell.scale = 20f;
+					foundCell.firstChunk.pos = vector;
+					foundCell.firstChunk.vel = Vector2.zero;
+				}
+
+				// Kill anything that gets too close
+				foreach (var abstractCreature in room.abstractRoom.creatures.Where(abstractCreature =>
+					         abstractCreature.creatureTemplate.type != CreatureTemplate.Type.Overseer &&
+					         abstractCreature.realizedCreature != null && !abstractCreature.state.dead &&
+					         Vector2.Distance(abstractCreature.realizedCreature.DangerPos, vector) < 100f))
+				{
+					room.AddObject(new ShockWave(abstractCreature.realizedCreature.DangerPos, 150f, 0.4f, 30));
+					for (int i = 0; i < 30; i++)
+					{
+						room.AddObject(new Spark(
+							abstractCreature.realizedCreature.DangerPos +
+							Custom.RNV() * UnityEngine.Random.value * 40f,
+							Custom.RNV() * Mathf.Lerp(4f, 30f, UnityEngine.Random.value), Color.white, null, 4,
+							18));
+					}
+
+					room.AddObject(new ElectricDeath.SparkFlash(abstractCreature.realizedCreature.DangerPos,
+						10.5f));
+					room.PlaySound(SoundID.Zapper_Zap, 0f, 0.8f, 0.5f);
+					abstractCreature.realizedCreature.Die();
+					abstractCreature.realizedCreature.Destroy();
+				}
+				return;
+			}
+			
+			// Cell activation is currently happening
+			if (finalPhase)
+			{
+				if (room.game.cameras[0].room != room)
+				{
+					Destroy();
+				}
+				return;
+			}
+			
+			// Cell has not been activated yet
+			switch (primed)
+			{
+				case false:
+				{
+					bool flag = false;
+					
+					// Check if a player is in the room
+					foreach (var t in room.game.Players.Where(t => t.Room == room.abstractRoom))
+					{
+						flag = true;
+					}
+					
+					// cell literally hasn't been found
+					if (foundCell != null)
+					{
+						while (foundCell.grabbedBy.Count > 0)
+						{
+							Creature grabber = foundCell.grabbedBy[0].grabber;
+							foundCell.grabbedBy[0].Release();
+							grabber.Stun(10);
+							grabber.firstChunk.vel += new Vector2(0f, -5f);
+						}
+					}
+					else
+					{
+						if (!flag)
+						{
+							return;
+						}
+
+						// Get teh active cell variable
+						using List<UpdatableAndDeletable>.Enumerator enumerator2 = room.updateList.GetEnumerator();
+						while (enumerator2.MoveNext())
+						{
+							UpdatableAndDeletable updatableAndDeletable = enumerator2.Current;
+							if (updatableAndDeletable is not EnergyCell cell) continue;
+							
+							foundCell = cell;
+							break;
+						}
+						if (foundCell == null)
+						{
+							noCellPresenceTime++;
+							return;
+						}
+						noCellPresenceTime = 0;
+						return;
+					}
+					foundCell.KeepOff();
+					primed = true;
+					return;
+				}
+				
+				// Cell is in room
+				case true when foundCell.room == room:
+				{
+					// Cell was just activated but animations still need to play
+					if (!(foundCell.usingTime > 0f)) return;
+					
+					foundCell.KeepOff();
+					foundCell.FireUp(room.MiddleOfTile(new IntVector2(14, 13)));
+					foundCell.AllGraspsLetGoOfThisObject(true);
+					
+					// Save data variables will track this now instead of room save data
+					(room.game.session as StoryGameSession)?.RemovePersistentTracker(foundCell.abstractPhysicalObject);
+					foundCell.canBeHitByWeapons = false;
+					// TODO: Implement save data
+					//room.game.GetStorySession.saveState.miscWorldSaveData.moonHeartRestored = true;
+					finalPhase = true;
+					return;
+				}
+				
+				// Cell isn't even in the room
+				case true when foundCell.room != room:
+					primed = false;
+					foundCell = null;
+					break;
+			}
 		}
-
-		private EnergyCell myEnergyCell;
-
-		public Player player;
-		
-		// Token: 0x04004083 RID: 16515
-		private bool primed;
-
-		// Token: 0x04004084 RID: 16516
-		private EnergyCell foundCell;
-
-		// Token: 0x04004085 RID: 16517
-		private bool finalPhase;
-
-		// Token: 0x04004086 RID: 16518
-		private bool lethalMode;
-
-		// Token: 0x04004087 RID: 16519
-		private int noCellPresenceTime;
 	}
 }
