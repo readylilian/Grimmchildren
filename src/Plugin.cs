9b1d2689d4d2d7ffc338e3ed5d99c4e15a0b06ae
ChaseKellow <clk3552@g.rit.edu>
2024-04-01 22:57:08 -0400

    Got the fucking events to work

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -7,6 +7,9 @@ using SlugBase.Features;
 using SlugTemplate.Hooks;
 using static SlugBase.Features.FeatureTypes;
 using System.IO;
+using IteratorKit.CMOracle;
+using static IteratorKit.CMOracle.CMOracleBehavior;
+using static IteratorKit.IteratorKit;
 
 namespace SlugTemplate
 {
@@ -37,7 +40,7 @@ namespace SlugTemplate
         {
 
             On.RainWorld.OnModsInit += RainWorld_OnModsInit;
-
+            
         }
 
        // public static readonly Oracle.OracleID SRS = new Oracle.OracleID("SRS", register: true);
@@ -51,10 +54,12 @@ namespace SlugTemplate
             // This hook can probably be deleted if we never load resources
             On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
             //On.OracleBehavior.Update += OracleBehavior_Update;
-
+            CMOracleBehavior.OnEventEnd += snowcatIterator.OnEventEnd;
+            CMOracleBehavior.OnEventStart += snowcatIterator.OnEventStart;
+            On.OracleBehavior.Update += snowcatIterator.OracleBehavior_Update;
             // Enable our custom hooks
             PlayerHooks.Init();
-            FireHooks.Init();
+           // 
             
             // Enables the options menu
             MachineConnector.SetRegisteredOI("GrimmChildrenMod", options);
---
+++ src/snowcatIterator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IteratorKit;
+using IteratorKit.CMOracle;
+using static IteratorKit.CMOracle.CMOracleBehavior;
+using static IteratorKit.CMOracle.OracleJSON.OracleEventsJson;
+using static IteratorKit.IteratorKit;
+using IteratorKit.Debug;
+using SlugTemplate.Hooks;
+
+namespace SlugTemplate
+{
+    
+
+
+    internal class snowcatIterator
+    {
+        static bool doFireball = false;
+        static int playerConversationCount = 0;
+
+        public static readonly Oracle.OracleID iteratorObject = new Oracle.OracleID("snowcat_iterator", register: true);
+        
+        public static void OracleBehavior_Update(On.OracleBehavior.orig_Update orig, OracleBehavior self, bool eu)
+        {
+            orig(self, eu);
+            if (self.oracle.ID == iteratorObject)
+            {
+                
+                if (doFireball)
+                {
+                    doFireball = false;
+                    playerConversationCount++;
+                    if (playerConversationCount > 1)
+                    {
+                        CMOracleBehavior cmBehavior = self as CMOracleBehavior;
+                        cmBehavior.cmConversation = new CMConversation(cmBehavior, CMConversation.CMDialogType.Generic, "fireballGift");
+                    }
+                    
+                }
+                
+
+            }
+        }
+
+        public static void OnEventEnd(CMOracleBehavior cMOracleBehavior, string eventName)
+        {
+            UnityEngine.Debug.Log("event ended " + eventName.Trim());
+            switch (eventName.Trim())
+            {
+                case "fireballGift":
+
+               
+                    cMOracleBehavior.roomGravity = 0.5f;
+                    FireHooks.Init();
+                    break;
+                case "playerConversation":
+
+                    UnityEngine.Debug.Log("tried fireball");
+                    doFireball = true;
+
+                    break;
+
+            } 
+        }
+        public static void OnEventStart(CMOracleBehavior cmBehavior, string eventName, OracleEventObjectJson eventData)
+        {
+           // UnityEngine.Debug.Log("Event started " + eventData.text);
+            //UnityEngine.Debug.Log("Event action " + eventData.action);
+            if (cmBehavior.oracle.ID == iteratorObject)
+            {
+               // Logger.LogInfo("event triggered " + eventName);
+                if (eventName == "myCustomEvent")
+                {
+                    // run code your own event code
+                }
+            }
+
+        }
+
+    }
+}
